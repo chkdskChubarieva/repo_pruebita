aac0a42d4f935c5cb67c145d10d520a0357474ab
Christian Arias <christian.chubarieva@gmail.com>
2025-02-08 17:14:06 -0400

    version antigua

--- src/pagepublic/PageHome.jsx
+++ src/pagepublic/PageHome.jsx
@@ -26,24 +26,22 @@ export default function PageHome() {
   return (
     <>
       <Header botones={botonesNavbar} />
-      <main id="home" className="relative top-20 -z-10 background w-full">
+      <main id="home" className="relative w-full top-20 -z-10 background">
         <section className="bg-black bg-opacity-20">
-          <h1 className="px-6 py-12 text-4xl font-semibold leading-tight text-white sm:px-12 sm:py-32 sm:text-5xl md:w-1/2 md:text-6xl font-inter font-light">
+          <h1 className="px-6 py-12 text-4xl font-light font-semibold leading-tight text-white sm:px-12 sm:py-32 sm:text-5xl md:w-1/2 md:text-6xl font-inter">
             GOMILOKOS
           </h1>
-          <div className="justify-end hidden md:flex px-6">
+          <div className="justify-end hidden px-6 md:flex">
             <h3 className="text-slate-200">El sabor de la lokura</h3>
           </div>
         </section>
 
-        <section id="caracteristicas" className="bg-white px-6 py-10 sm:px-12">
+        <section id="caracteristicas" className="px-6 py-10 bg-white sm:px-12">
           <Caracteristicas />
-        </section>
-        <div className="bg-white flex justify-center ">
+        <div className="flex justify-center pb-12 bg-white">
            <FourLokoCarousel/>
         </div>
-
-        <Footer />
+        </section>
 
       </main>
       <div id="contacto">
